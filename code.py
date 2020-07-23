36ca3848ec47ecc407adbe1fee95fff42a2b6452
Joel Huffman <huffmajo@oregonstate.edu>
2020-07-22 20:42:06 -0700

    Added feedback sounds for specific actions. Revised food token/feed schedule.

--- code.py
+++ code.py
@@ -1,21 +1,35 @@
 # AUTOMATED CAT FOOD DISPENSER
 # AUTHOR: Joel Huffman
-# LAST UPDATED: 7/18/2020
+# LAST UPDATED: 7/22/2020
 # PURPOSE: Allow cats to dispense serve themselves dry food pellets by depressing a
 # push button. To prevent gourging. food will be dispensed in small quantites and only
 # if specific criteria are met.
-# RULES: The day starts with 10 food tokens at 6AM
-# - At 6AM the number of food tokens is set to 10
-# - Each button press removes a food token, dispenses 1 oz of dry food and plays sound
-# - If no food tokens remain, a button press only plays a sad trombone sound
-# - If a button press occurs within 10 minutes of a prior button press it does not 
-# remove a food token or dispense food
-
+# RULES: The feeder has 2 states: ready and cooldown.
+# - In ready state, if button is pressed, a small portion of food will be dispensed,
+# a positive sound will be played, and the feeder will switch to cooldown state.
+# - In cooldown state, button presses will play a negative sound, flash error lights 
+# and NOT dispense food.  
+# - After 4 hours in cooldown state, the feeder will revert to ready state and play 
+# a dinner bell sound.
 
 from time import sleep, monotonic
 import board
 from digitalio import DigitalInOut, Pull, Direction
 from neopixel import NeoPixel
+
+# audio setup
+try:
+    from audiocore import WaveFile
+except ImportError:
+    from audioio import WaveFile
+ 
+try:
+    from audioio import AudioOut
+except ImportError:
+    try:
+        from audiopwmio import PWMAudioOut as AudioOut
+    except ImportError:
+        pass  # not always supported by every board!
 
 # color rgb values
 OFF = (0, 0, 0)
@@ -42,6 +56,11 @@ button_feed.pull = Pull.UP
 # motor controller
 motor = DigitalInOut(board.A2)
 motor.direction = Direction.OUTPUT
+
+# enable audio speaker
+speaker = DigitalInOut(board.SPEAKER_ENABLE)
+speaker.direction = Direction.OUTPUT
+speaker.value = True
 
 # blink all neopixels red 3 times
 def errorBlink():
@@ -72,9 +91,18 @@ def lightLeds():
             pixels[i] = OFF
     pixels.show()
 
+# play .wav file
+def playFile(filename):
+    wave_file = open(filename, "rb")
+    with WaveFile(wave_file) as wave:
+        with AudioOut(board.SPEAKER) as audio:
+            audio.play(wave)
+            while audio.playing:
+                pass
+
 food_tokens = 10
 food_tokens_max = 10
-timer_5_min = 60
+timer_4_hours = 14400
 initial = monotonic()
 
 while True:
@@ -84,16 +112,19 @@ while True:
         if food_tokens > 0:
             food_tokens = food_tokens-1
             led.value = True
+            playFile("chime.wav")
             runMotor(0.1)
         else:
+            playFile("chime.wav")
             errorBlink()
         print(food_tokens)
     else:
         led.value = False
 
-    if now - initial > timer_5_min:
+    if now - initial > timer_4_hours:
         if food_tokens < food_tokens_max:
             food_tokens = food_tokens+1
+            playFile("come_get_it.wav")
         initial = now
         print(food_tokens)
     
